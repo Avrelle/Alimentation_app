cf392f07a140d273b39ceaec7e3815d11d01bbfa
Avrelle <anto.delahaye@gmail.com>
2022-12-16 17:01:15 +0100

    add login and register form

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,3 +36,15 @@ const ctx = document.getElementById('myChart');
         }
     }
   });
+
+
+
+function sliderChangeSize(val){
+    document.getElementById("output").innerHTML = val;
+}
+document.getElementById('size').value =170;
+
+function sliderChangeWeight(val){
+    document.getElementById("outputBis").innerHTML = val;
+}
+document.getElementById('weight').value =70;
--- login.php
+++ login.php
@@ -9,10 +9,47 @@ include_once("includes/header.php");
 ?>
 
 
-<h1>Login Page!</h1>
 
 
-<?php include_once('includes/footer.php');?>
 
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Login Page!</h1>
 
-    
+            </div>
+        </div>
+    </div>
+
+</header>
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <form>
+                    <div class="mb-3">
+                        <label for="email" class="form-label mt-3">Email</label>
+                        <input type="email" class="form-control" id="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Mot de passe</label>
+                        <input type="password" class="form-control" id="password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+
+                <a href="register.php">
+                    <button type="button" class="btn btn-primary mt-2">
+                        register
+                    </button>
+                </a>
+
+            </div>
+        </div>
+    </div>
+
+</main>
+<footer>
+    <?php include_once('includes/footer.php');?>
+</footer>
